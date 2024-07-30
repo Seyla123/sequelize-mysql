0b05e880e53c9d14a5382bae9a9efc519cbf5404
seav seyla <Mrrseyla.758@gmail.com>
2024-07-30 17:10:42 +0700

    add sequelize

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
-const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+const { Sequelize ,DataTypes} = require('sequelize');
 
-const sequelize = new Sequelize('database-1.cvk2g82gk1s1.us-east-2.rds.amazonaws.com', 'admin', 'password', {
-    host: 'localhost',
+dotenv.config(); // Load environment variables
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+    host: process.env.DB_HOST,
     dialect: 'mysql'
   });
+  sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+ }).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+ });
 
+ async function fetchAllClasses() {
+  try {
+    // Use sequelize.query to execute raw SQL
+    const [results, metadata] = await sequelize.query('SELECT * FROM Class');
+    
+    console.log('All classes:', JSON.stringify(results, null, 2));
+  } catch (error) {
+    console.error('Error fetching classes:', error);
+  }
+}
+
+fetchAllClasses();
